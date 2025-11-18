8ed2fcca49df06a773e6d5026f91366c121e2cb3
Franck D'agostini <franck.dagostini@gmail.com>
2025-11-18 09:36:22 +0100

    Fix rubocop linting issues
    - Remove trailing whitespace
    - Fix array bracket spacing
    - Use double-quoted strings consistently

--- app/helpers/observ/pagination_helper.rb
+++ app/helpers/observ/pagination_helper.rb
@@ -6,7 +6,7 @@ module Observ
     # Usage: <%= observ_pagination(@collection) %>
     def observ_pagination(collection)
       return unless collection.respond_to?(:current_page)
-      
+
       content_tag(:div, class: "observ-pagination") do
         safe_join([
           pagination_info(collection),
@@ -14,24 +14,24 @@ module Observ
         ])
       end
     end
-    
+
     private
-    
+
     def pagination_info(collection)
       return "" if collection.total_count.zero?
-      
+
       from = collection.offset_value + 1
-      to = [collection.offset_value + collection.limit_value, collection.total_count].min
+      to = [ collection.offset_value + collection.limit_value, collection.total_count ].min
       total = collection.total_count
-      
+
       content_tag(:div, class: "observ-pagination__info") do
         "Showing #{from}-#{to} of #{total}"
       end
     end
-    
+
     def pagination_links(collection)
       content_tag(:div, class: "observ-pagination__links") do
-        paginate collection, theme: 'observ'
+        paginate collection, theme: "observ"
       end
     end
   end
--- spec/helpers/observ/pagination_helper_spec.rb
+++ spec/helpers/observ/pagination_helper_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe Observ::PaginationHelper, type: :helper do
   let(:collection) { double("Collection") }
-  
+
   describe "#observ_pagination" do
     context "when collection is not paginated" do
       it "returns nil" do
@@ -12,7 +12,7 @@ RSpec.describe Observ::PaginationHelper, type: :helper do
         expect(helper.observ_pagination(collection)).to be_nil
       end
     end
-    
+
     context "when collection is paginated" do
       before do
         allow(collection).to receive(:respond_to?).with(:current_page).and_return(true)
@@ -22,23 +22,23 @@ RSpec.describe Observ::PaginationHelper, type: :helper do
         allow(collection).to receive(:current_page).and_return(1)
         allow(helper).to receive(:paginate).and_return("<pagination links>")
       end
-      
+
       it "returns pagination wrapper" do
         result = helper.observ_pagination(collection)
         expect(result).to include('observ-pagination')
       end
-      
+
       it "includes pagination info" do
         result = helper.observ_pagination(collection)
         expect(result).to include('Showing 1-25 of 100')
       end
-      
+
       it "includes pagination links" do
         result = helper.observ_pagination(collection)
         expect(result).to include('observ-pagination__links')
       end
     end
-    
+
     context "when collection is empty" do
       before do
         allow(collection).to receive(:respond_to?).with(:current_page).and_return(true)
@@ -47,13 +47,13 @@ RSpec.describe Observ::PaginationHelper, type: :helper do
         allow(collection).to receive(:limit_value).and_return(25)
         allow(helper).to receive(:paginate).and_return("")
       end
-      
+
       it "does not show pagination info" do
         result = helper.observ_pagination(collection)
         expect(result).not_to include('Showing')
       end
     end
-    
+
     context "when on second page" do
       before do
         allow(collection).to receive(:respond_to?).with(:current_page).and_return(true)
@@ -63,13 +63,13 @@ RSpec.describe Observ::PaginationHelper, type: :helper do
         allow(collection).to receive(:current_page).and_return(2)
         allow(helper).to receive(:paginate).and_return("<pagination links>")
       end
-      
+
       it "shows correct range" do
         result = helper.observ_pagination(collection)
         expect(result).to include('Showing 26-50 of 100')
       end
     end
-    
+
     context "when on last partial page" do
       before do
         allow(collection).to receive(:respond_to?).with(:current_page).and_return(true)
@@ -79,7 +79,7 @@ RSpec.describe Observ::PaginationHelper, type: :helper do
         allow(collection).to receive(:current_page).and_return(4)
         allow(helper).to receive(:paginate).and_return("<pagination links>")
       end
-      
+
       it "shows correct range for partial page" do
         result = helper.observ_pagination(collection)
         expect(result).to include('Showing 76-78 of 78')
